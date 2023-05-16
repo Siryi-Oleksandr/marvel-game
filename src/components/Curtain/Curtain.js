d04e4272462bc4891f9c8ab93a724581b0ccb1ca
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2023-05-16 08:28:10 +0300

    adds & refactor
     - refactor curtain
     - adds welcomeBtn

---
+++ src/components/Curtain/Curtain.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './Curtain.scss';
+import { Box } from '@chakra-ui/react';
+import { WelcomeBtn } from 'components';
+
+export const Curtain = ({ setHomePage }) => {
+  return (
+    <Box class="curtainBody" overflow="hidden" w="100vw" h="100vh">
+      <Box
+        w="50vw"
+        h="100vh"
+        position="absolute"
+        id="leftCurtain"
+        left="-50vw"
+        zIndex="10"
+        className="curtainContainer"
+        scroll="no-scroll"
+      >
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+      </Box>
+      <Box
+        h="100vh"
+        w="100vw"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        className="buttonContainer"
+        position="relative"
+      >
+        <WelcomeBtn setHomePage={setHomePage} />
+      </Box>
+      <Box
+        w="50vw"
+        h="100vh"
+        position="absolute"
+        zIndex="10"
+        right="-50vw"
+        className="curtainContainer"
+        id="rightCurtain"
+      >
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+        <Box class="unCurtain"></Box>
+      </Box>
+    </Box>
+  );
+};
