1e115ce2014e39ebf4a9457de9ca22c6f034caa0
Iryna Grytsaenko <iryna.grytsaenko@gmail.com>
2023-05-22 18:28:01 +0300

    slight refactor
    slight refactor

--- src/pages/SharedLayout/SharedLayout.js
+++ src/pages/SharedLayout/SharedLayout.js
@@ -1,8 +1,8 @@
 import { Box } from '@chakra-ui/react';
 import { Suspense } from 'react';
 import Loader2 from 'components/Loader2/Loader';
-import { NavLink, Outlet } from 'react-router-dom';
-import './SharedLayout.scss';
+import { Outlet } from 'react-router-dom';
+import { Tab } from 'components';
 
 export const SharedLayout = ({ sharedLay }) => {
   return (
@@ -10,16 +10,9 @@ export const SharedLayout = ({ sharedLay }) => {
       <>
         {sharedLay && (
           <Box display="flex" justifyContent="space-evenly" w="100vw" gap="0px">
-            <Box w="100%" as={NavLink} to="/play" className="nav-link">
-              Create your own team
-            </Box>
-
-            <Box as={NavLink} w="100%" to="/teamplay" className="nav-link">
-              Choose your team
-            </Box>
-            <Box as={NavLink} w="100%" to="/statistics" className="nav-link">
-              Statistics
-            </Box>
+            <Tab to="/play">Create your own team</Tab>
+            <Tab to="/teamplay">Choose your team</Tab>
+            <Tab to="/statistics">Statistics</Tab>
           </Box>
         )}
         <Suspense fallback={<Loader2 />}>
