acf4fb0184a198ca92299929d6db19889d533f6a
andrsvsrg <androsov.srg@gmail.com>
2023-05-22 10:51:35 +0300

    fix unused variables

--- src/pages/PlayTeamGame/PlayTeamGame.js
+++ src/pages/PlayTeamGame/PlayTeamGame.js
@@ -1,15 +1,11 @@
 import React, { useEffect } from 'react'
 import ChooseTeamList from '../../components/ChooseTeamList/ChooseTeamList'
 import { useDispatch } from 'react-redux'
-import { addCardToTeam, deleteAllHeroes } from '../../redux/cards/slice'
 import SelectedTeam from '../../components/TeamList/TeamList'
 import './PlayTeamGame.scss'
 
 
 export const PlayTeamGame = () => {
-
-
-  const dispatch = useDispatch()
 
   // function isAllHeroesFromSameTeam() {
   //   if(userTeam.length === 0) return false
